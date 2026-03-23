7ebce18eb047053324f4b4bbd22a1a61dd2e98d9
david-vde <vanderelst.david@gmail.com>
2026-03-23 20:12:44 +0100

    Fix build error due to unexisting backend yet

--- frontend/vite-plugin-config-inject.js
+++ frontend/vite-plugin-config-inject.js
@@ -4,10 +4,8 @@ export default function configInjectPlugin() {
   return {
     name: 'config-inject',
     async transformIndexHtml(html) {
-      const configs = await retrieveConfig(process.env.BACKEND_URL);
-      const personName = configs['contact.firstname'] + " " + configs['contact.lastname'];
       return html
-        .replace('{{PAGE_TITLE}}', personName + " - Chatbot CV");
+        .replace('{{PAGE_TITLE}}', process.env.CHATBOT_PERSON_NAME + " - Chatbot CV");
     }
   };
 }
